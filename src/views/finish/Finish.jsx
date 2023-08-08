413f48ec576df4b3fb2e3c448568451306290e18
Nahuel Lautaro Torres Loretto <112157731+Lautaro261@users.noreply.github.com>
2023-08-08 01:00:06 -0300

    finish casi listo

--- src/views/finish/Finish.jsx
+++ src/views/finish/Finish.jsx
@@ -6,11 +6,15 @@ const Finish =()=>{
     return(
         <div className={style.parent}>
             <div className={style.div1}>
-                <h1 className={style.frase}>Un código es poderoso no porque funcione, sino porque puede resolver problemas.</h1>
+                <h1 className={style.frase1}>Un código es poderoso no</h1>
+                <h1 className={style.frase1}>porque funcione, sino porque </h1>
+                <h1 className={style.frase1}>puede expresar ideas.</h1>
             </div>
 
             <div className={style.div2}>
-                <h4>frase 2</h4>
+                <h4 className={style.frase2}>Mi abuelo me solía decir esto </h4>
+                <h4 className={style.frase2}>cuando quería abandonar </h4>
+                <h4 className={style.frase2}>la idea de ser programador.</h4>
             </div>
             
         </div>
