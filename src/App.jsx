3f738a68f08452c72aec90814e574a953637d8d2
Nahuel Lautaro Torres Loretto <112157731+Lautaro261@users.noreply.github.com>
2023-09-08 20:08:22 -0300

    funcionalidad lista scroll con router

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,20 @@
-import { useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { Element, Link as ScrollLink } from 'react-scroll';
-import Landing from './views/landing/landing';
-import AboutMe from './views/aboutMe/aboutMe';
-import Projects from './views/projects/projects';
-import Finish from './views/finish/Finish';
-import Detail from './views/detail/detail';
-import style from './App.module.css';
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Detail from "./views/detail/detail";
+import Home from "./views/home/Home";
+import style from "./App.module.css";
 
 function App() {
   return (
-    <div>
-      <header>
-        <div className={style.containerNavBar}>
-
-
-        <nav className={style.navBar}>
-
-          <ul className={style.containerList}>
-
-            <li className={style.item}>
-              <ScrollLink to="landing" smooth={true} offset={-50} duration={500}>
-                Inicio
-              </ScrollLink>
-            </li>
-
-            <li className={style.item}>
-              <ScrollLink to="projects" smooth={true} offset={0} duration={750}>
-                Proyectos
-              </ScrollLink>
-            </li>
-            <li className={style.item}>
-              <ScrollLink to="aboutme" smooth={true} offset={0} duration={750}>
-                Sobre mi
-              </ScrollLink>
-            </li>
-            <li className={style.item}>
-              <ScrollLink to="finish" smooth={true} offset={0} duration={750}>
-                Contacto
-              </ScrollLink>
-            </li>
-          </ul>
-        </nav>
-        </div>
-      </header>
-
-      <main>
-        <Element name="landing">
-          <div className={style.content}>
-            <Landing className={style.contentHeader} />
-          </div>
-        </Element>
-
-        <Element name="projects">
-          <div className={style.content}>
-            <Projects className={style.contentHeader} />
-          </div>
-        </Element>
-
-        <Element name="aboutme">
-          <div className={style.content}>
-            <AboutMe className={style.contentHeader} />
-          </div>
-        </Element>
-
-        <Element name="finish">
-          <div className={style.content}>
-            <Finish className={style.contentHeader} />
-          </div>
-        </Element>
-      </main>
-    </div>
-  );
-}
-
-export default App;
-
-
-
-/* function App() {
-
-  return (
     <Routes>
-      <Route path='/' element={<Landing/>}/>  {/*1 Landing /}
-      <Route path='/projects' element={<Projects/>}/> {/*2 projects Web /}
-      <Route path='/finish' element={<Finish/>}/>     {/* 3 Finish /}
-      <Route path='/projects/detail/:id' element={<Detail/>}/>  {/* 5 Detail /}
-      <Route path='/aboutme' element={<AboutMe/>}/> {/*6 AboutMe /}
+      <Route path="/" element={<Home />} /> {/*1 Landing */}
+      <Route path="/projects/detail/:id" element={<Detail />} />
+      {/* 5 Detail */}
     </Routes>
-  )
+  );
 }
 
 // FALTA
 // 4 Projects Apps
 
-export default App */
+export default App;
---
+++ src/views/home/Home.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { Element, Link as ScrollLink } from "react-scroll";
+import Landing from "../landing/landing";
+import Projects from "../projects/projects";
+import AboutMe from "../aboutMe/aboutMe";
+import Finish from "../finish/Finish";
+import style from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <div>
+      <header>
+        <div className={style.containerNavBar}>
+          <nav className={style.navBar}>
+            <ul className={style.containerList}>
+              <li className={style.item}>
+                <ScrollLink
+                  to="landing"
+                  smooth={true}
+                  offset={-50}
+                  duration={500}
+                >
+                  Inicio
+                </ScrollLink>
+              </li>
+
+              <li className={style.item}>
+                <ScrollLink
+                  to="projects"
+                  smooth={true}
+                  offset={0}
+                  duration={750}
+                >
+                  Proyectos
+                </ScrollLink>
+              </li>
+              <li className={style.item}>
+                <ScrollLink
+                  to="aboutme"
+                  smooth={true}
+                  offset={0}
+                  duration={750}
+                >
+                  Sobre mi
+                </ScrollLink>
+              </li>
+              <li className={style.item}>
+                <ScrollLink to="finish" smooth={true} offset={0} duration={750}>
+                  Contacto
+                </ScrollLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+
+      <main>
+        <Element name="landing">
+          <div className={style.content}>
+            <Landing className={style.contentHeader} />
+          </div>
+        </Element>
+
+        <Element name="projects">
+          <div className={style.content}>
+            <Projects className={style.contentHeader} />
+          </div>
+        </Element>
+
+        <Element name="aboutme">
+          <div className={style.content}>
+            <AboutMe className={style.contentHeader} />
+          </div>
+        </Element>
+
+        <Element name="finish">
+          <div className={style.content}>
+            <Finish className={style.contentHeader} />
+          </div>
+        </Element>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
